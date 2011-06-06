9b05804d9adb2bb945a67f8fdc7af2cbba16b225
Zach Mullen <zach.mullen@kitware.com>
2011-06-06 14:04:46 -0400

    ENH: Add external project exports

---
+++ GenerateSQLiteConfig.cmake
@@ -0,0 +1,19 @@
+CONFIGURE_FILE(
+  ${sqlite_SOURCE_DIR}/Usesqlite.cmake.in
+  ${sqlite_BINARY_DIR}/Usesqlite.cmake COPYONLY)
+
+# Library directory
+SET(sqlite_LIBRARY_DIRS_CONFIG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+# Include directories
+SET(sqlite_INCLUDE_DIRS_CONFIG
+  ${sqlite_SOURCE_DIR}
+  )
+
+# Usesqlite file
+SET(sqlite_USE_FILE_CONFIG ${PROJECT_BINARY_DIR}/Usesqlite.cmake)
+
+# Configure sqliteConfig.cmake
+CONFIGURE_FILE(
+  ${sqlite_SOURCE_DIR}/sqliteConfig.cmake.in
+  ${sqlite_BINARY_DIR}/sqliteConfig.cmake @ONLY)
